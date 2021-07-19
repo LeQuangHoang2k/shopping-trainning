85fb23fe89105eb578b8c37b473b653de7335c2e
your username <theibdn159555@gmail.com>
2021-07-19 11:15:26 +0700

    add repository

--- laravel/app/Http/Controllers/CategoriesController.php
+++ laravel/app/Http/Controllers/CategoriesController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class CategoriesController extends Controller
 {
-
-    public function __construct()
-    {
-        # code...
-    }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         //
