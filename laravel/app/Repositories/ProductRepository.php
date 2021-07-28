19e253cb2a5b67bfe35c4dee7fdecc705f14db15
your username <theibdn159555@gmail.com>
2021-07-28 16:06:57 +0700

    fix api show

--- laravel/app/Repositories/ProductRepository.php
+++ laravel/app/Repositories/ProductRepository.php
@@ -15,10 +15,10 @@ class ProductRepository
 
         if ($filters == []) return [];
 
-        //find 1
-        if (isset($filters['id'])) {
-            return $query->where('id', $filters['id'])->get();
-        }
+        // //find 1
+        // if (isset($filters['id'])) {
+        //     return $query->where('id', $filters['id'])->get();
+        // }
 
         if (isset($filters['page']) || is_null($filters['page'])) {
             $validator = Validator::make($filters, [
