6abbd915f02a31295e3d7f373172c865326ea49d
your username <theibdn159555@gmail.com>
2021-07-28 09:01:16 +0700

    fix sth

--- laravel/app/Http/Resources/ProductResource.php
+++ laravel/app/Http/Resources/ProductResource.php
@@ -19,7 +19,7 @@ class ProductResource extends JsonResource
             "id" => $this->id,
             "name" => $this->name,
             "slug" => $this->slug,
-            "decription" => $this->decription,
+            "description" => $this->description,
             "price" => $this->price,
             "picture" => $this->picture,
             "priority" => $this->priority,
