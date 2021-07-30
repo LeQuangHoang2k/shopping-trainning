50b2490e88316167d67c42f0a4a81215ad9a8040
your username <theibdn159555@gmail.com>
2021-07-30 13:05:02 +0700

    bugs expires_in

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -4,80 +4,57 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Repositories\UserRepository;
+use Illuminate\Support\Carbon;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $userRepository;
+
+    public function __construct(UserRepository $userRepository)
     {
         $this->middleware('auth:api', ['except' => ['login']]);
+        $this->userRepository = $userRepository;
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
+    public function login(LoginRequest $request)
     {
-        // dd("123");
-        $credentials = request(['email', 'password']);
-        if (! $token = auth()->attempt($credentials)) {
+        $credentials = $request->validated();
+
+        if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        return $this->respondWithToken($token, $credentials);
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function me()
     {
         return response()->json(auth()->user());
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function logout()
-    // {
-    //     auth()->logout();
+    public function logout()
+    {
+        // auth()->logout();
 
-    //     return response()->json(['message' => 'Successfully logged out']);
-    // }
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function refresh()
-    // {
-    //     return $this->respondWithToken(auth()->refresh());
-    // }
+    public function refresh()
+    {
+        // return $this->respondWithToken(auth()->refresh(),a);
+    }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
+    protected function respondWithToken($token, $credentials)
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            // 'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' =>  Carbon::today()->toDateTimeString(),
+            // 'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => new UserResource($this->userRepository->find($credentials)),
         ]);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -32,9 +32,9 @@ Route::resource('/register',  RegisterController::class);
 Route::resource('products', ProductController::class);
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', 'AuthController@logout');
-Route::post('refresh', 'AuthController@refresh');
-Route::post('me', 'AuthController@me');
+Route::post('logout',  [AuthController::class, 'logout']);
+Route::post('refresh',  [AuthController::class, 'refresh']);
+Route::post('me',  [AuthController::class, 'me']);
 
 Route::get('/test',  [AuthController::class, 'test']);
 
