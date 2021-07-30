d9ca0d4430be396e137ddfc93b5c8e5313dde9fb
your username <theibdn159555@gmail.com>
2021-07-30 11:52:48 +0700

    fix user modsel

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements JWTSubject
 {
     // use HasFactory;
+    use Notifiable;
 
     protected $fillable = [
         "id",
