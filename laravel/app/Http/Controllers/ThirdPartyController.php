f71d60ae10b69dd9604e8a4025209ad782088b9b
your username <theibdn159555@gmail.com>
2021-08-02 06:29:51 +0700

    fix sth

--- laravel/app/Http/Controllers/ThirdPartyController.php
+++ laravel/app/Http/Controllers/ThirdPartyController.php
@@ -9,17 +9,41 @@ use App\Models\User;
 use App\Services\ThirdParty;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\isNull;
+
 class ThirdPartyController extends Controller
 {
     public function registerFacebook(RegisterFacebookRequest $request)
     {
         $credentials = $request->validated();
-        $userDB =  User::where('facebook_id', $credentials['facebook_id'])->first();
+        $user = null;
+        $userDB =  User::where('email', $credentials['email'])->first();
+        $newName = $userDB->name;
+        $newPicture = $userDB->picture;
 
+        //check xem email này có phải mình ko
         if (!$userDB) {
             //create
+            $user = User::create($credentials);
         } else {
-            //create
+            // thông báo email này đã tồn tại
+            if (!isset($credentials['is_duplicate'])) {
+                return response()->json(["message_duplicate" => "email này đã được đăng kí, đây có phải bạn ko ?."]);
+            }
+
+            // client trả lời có
+            if ($credentials['is_duplicate']) {
+                // update
+                // dd("update");
+                if (!isNull($newName) && !isNull($newPicture)) return $user;
+                if (isNull($newName)) $newName = $credentials['name'];
+                if (isNull($newPicture)) $newPicture = $credentials['picture'];
+
+                $user = User::where('email', $credentials['email'])->update(['name' => $newName, 'picture' => $newPicture]);
+            } else {
+                // dd("create");
+                $user = User::create($credentials);
+            }
         }
 
         // gen access token
