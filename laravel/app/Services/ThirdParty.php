dcf1b3d37aea1a918990b7c5d306ba3fb64ce18f
your username <theibdn159555@gmail.com>
2021-08-02 04:29:34 +0700

    fix sth

--- laravel/app/Services/ThirdParty.php
+++ laravel/app/Services/ThirdParty.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Models\User;
 
+use function PHPUnit\Framework\isNull;
+
 class ThirdParty
 {
     public function syncAccountFacebook($credentials)
@@ -11,6 +13,8 @@ class ThirdParty
         // dd("syncAccountFacebook");
 
         $userDB =  User::where('email', $credentials['email'])->first();
+        $newName = $userDB->name;
+        $newPicture = $userDB->picture;
         $user = null;
 
         if ($userDB) {
@@ -19,8 +23,12 @@ class ThirdParty
                 return print_r(json_encode(["error" => "Unconfirmed duplicate !"]));
 
             if ($credentials["is_duplicate"]) {
-                // dd("update");
-                User::where('email', $credentials['email']);
+
+                if (!isNull($newName) && !isNull($newPicture)) return $user;
+                if (isNull($newName)) $newName = $credentials['name'];
+                if (isNull($newPicture)) $newPicture = $credentials['picture'];
+
+                $user = User::where('email', $credentials['email'])->update(['name' => $newName, 'picture' => $newPicture]);
             } else {
                 dd("create");
             }
@@ -28,9 +36,7 @@ class ThirdParty
             $user = User::create($credentials);
         }
 
-        //get user
-
-        // $user = "";
+        dd("pass");
 
         return $user;
     }
