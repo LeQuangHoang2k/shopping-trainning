accbd75678f7d17bc403f37db9a130035999320f
your username <theibdn159555@gmail.com>
2021-08-01 21:16:35 +0700

    fix sth

--- laravel/app/Services/ThirdParty.php
+++ laravel/app/Services/ThirdParty.php
@@ -8,19 +8,26 @@ class ThirdParty
 {
     public function syncAccountFacebook($credentials)
     {
-        dd("syncAccountFacebook");
-        
-        //!exist => create
-        
-        // $user = User::create($credentials);
+        // dd("syncAccountFacebook");
+        $user =  User::where('email', $credentials['email'])->first();
+        // dd($user);
 
-        //exist && different user => create 
-        
-        //exist && same user => update 
+        if (!$user) {
+            //!exist => create
+            // dd($user);
+            $user = User::create($credentials);
+        } else if ($user) {
+            //exist && different user => create 
+            dd($user);
+        } else {
+            //exist && same user => update 
+            dd($user);
+        }
+
 
         //get user
 
-        $user = "";
+        // $user = "";
 
         return $user;
     }
