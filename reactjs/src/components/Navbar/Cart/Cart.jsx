9868a443874ebaf353ee53106d5b533b92854be5
your username <theibdn159555@gmail.com>
2021-08-10 16:29:55 +0700

    version ổn định

--- reactjs/src/components/Navbar/Cart/Cart.jsx
+++ reactjs/src/components/Navbar/Cart/Cart.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Cart.css";
 // import CartIcon from "/images/CartIcon.png";
 
 function Cart(props) {
+  const [amount, setAmount] = useState(1);
   const showCart = () => {
     window.location.href = "/cart";
   };
@@ -16,7 +17,10 @@ function Cart(props) {
         alt="Image"
       />
       <div className="cart_navbar_infor">
-        <div className="cart_navbar_amount">10</div>
+        {/* {amount > 0 ? amount : ""} */}
+        <div className="cart_navbar_box">
+          <div className="cart_navbar_amount" style={{ display: amount > 0 ? "block" : "none" }}>{amount}</div>
+        </div>
         <div className="cart_navbar_title">Cart</div>
       </div>
     </div>
