fa3eb232e3bee94aa39ba4ea90642f7b4030f47b
your username <theibdn159555@gmail.com>
2021-08-13 14:40:23 +0700

    return order list

--- reactjs/src/components/Navbar/Account/DropdownUser/DropdownUser.jsx
+++ reactjs/src/components/Navbar/Account/DropdownUser/DropdownUser.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 import Logout from "./Logout/Logout";
+import Order from "./Order/Order";
+
 import "./DropdownUser.css";
 
 function DropdownUser(props) {
-    
-    return (
-        <div className="account_dropdown">
-            <Logout/>
-        </div>
-    );
+  return (
+    <div className="account_dropdown">
+      <Logout />
+
+      <Order />
+    </div>
+  );
 }
 
 export default DropdownUser;
-
-
---
+++ reactjs/src/components/Navbar/Account/DropdownUser/Order/Order.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "./Order.css";
+
+function Order(props) {
+  return <Button variant="account_order">Order</Button>;
+}
+
+export default Order;
