9a9710974086032156209ff8d1de48a217d9b7f6
your username <theibdn159555@gmail.com>
2021-08-16 10:49:46 +0700

    fix db

---
+++ laravel/database/migrations/2021_08_16_033950_create_discount_codes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->id();
+            $table->integer("product_id");
+            $table->string("code")->nullable();
+            $table->float("rate")->nullable();
+            $table->boolean("active")->nullable();
+            $table->dateTime("expired_at")->nullable();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_codes');
+    }
+}
