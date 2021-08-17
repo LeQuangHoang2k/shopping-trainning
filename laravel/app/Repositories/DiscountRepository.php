8b7f78a06e5becd55e8d256e792f88dadf52c232
your username <theibdn159555@gmail.com>
2021-08-17 19:24:10 +0700

    up 29

--- laravel/app/Repositories/DiscountRepository.php
+++ laravel/app/Repositories/DiscountRepository.php
@@ -9,8 +9,14 @@ class DiscountRepository
     public function get($filters)
     {
         $query  = Discount::select("*")->where('code', $filters['code'])->get();
+        // $query  = Discount::select("*")->where('code', 's')->get();
 
-        // dd($query);
+
+        // dd(count($query));
+        if (count($query) === 0) return ['error' => 'This code is not exist', 'data' => []];
+        if ($query[0]->is_used) return ['error' => 'This code is already in use', 'data' => []];
+        // if ($query[0]->is_expired) return ['error' => 'This code is not exist', 'data' => $query];
+
 
         return $query;
     }
