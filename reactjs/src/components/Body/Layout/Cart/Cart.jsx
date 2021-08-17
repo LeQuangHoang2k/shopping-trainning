3818f928aaea215b2d00327fbf9c2a7356ad6aca
your username <theibdn159555@gmail.com>
2021-08-17 14:44:33 +0700

    fix UI

--- reactjs/src/components/Body/Layout/Cart/Cart.jsx
+++ reactjs/src/components/Body/Layout/Cart/Cart.jsx
@@ -15,6 +15,7 @@ import {
 } from "../../../../redux/actions/cart";
 
 import "./Cart.css";
+import Summary from "./Summary/Summary";
 
 function Cart(props) {
   const cookies = new Cookies();
@@ -24,9 +25,6 @@ function Cart(props) {
 
   const [orderList, setOrderList] = useState([]);
   const [total, setTotal] = useState(0.0);
-  const [showLabel, setShowLabel] = useState(true);
-  const [success, setSuccess] = useState(false);
-  const [fail, setFail] = useState(false);
 
   useEffect(() => {
     console.log("cartStorage", cartStorage);
@@ -162,36 +160,11 @@ function Cart(props) {
     console.log("update cart", orderList, cartStorage.list);
   };
 
-  const checkCode = (e) => {
-    var value = e.target.value.length;
-
-    switch (value) {
-      case 0: {
-        setShowLabel(true);
-        setFail(false);
-        setSuccess(false);
-        break;
-      }
-
-      case 10: {
-        setShowLabel(false);
-        setFail(false);
-        setSuccess(true);
-        break;
-      }
-
-      default:
-        setShowLabel(false);
-        setFail(true);
-        setSuccess(false);
-        break;
-    }
-  };
-
   return (
     <div className="cart_side">
       <div className="card">
         <div className="row">
+          
           <div className="col-md-8 cart">
             <div className="title">
               <div className="row">
@@ -322,134 +295,8 @@ function Cart(props) {
               </span>
             </div>
           </div>
-          <div className="col-md-4 summary">
-            <div>
-              <h5>
-                <b>Summary</b>
-              </h5>
-            </div>
-            <hr />
 
-            <form>
-              <p>GIVE CODE</p>
-
-              <div className="div_animation">
-                <input id="code" onChange={checkCode} />
-                <label
-                  style={{ display: showLabel ? "block" : "none" }}
-                  htmlFor="code"
-                ></label>
-              </div>
-            </form>
-
-            <div
-              // className="row"
-              style={{
-                padding: "2vh 0",
-              }}
-            >
-              <div
-                style={{
-                  display: success ? "block" : "none",
-                  color: "lightgreen",
-                }}
-                className="col"
-              >
-                Validated!
-              </div>
-              <div
-                className="col"
-                style={{
-                  display: fail ? "block" : "none",
-                  color: "red",
-                }}
-              >
-                Not Validated!
-              </div>
-            </div>
-
-            <div
-              className="row"
-              style={{
-                // borderTop: "1px solid rgba(0,0,0,.1)",
-                padding: "2vh 0",
-              }}
-            >
-              <div className="col">Original Price</div>
-              <div className="col text-right">
-                {total > 0
-                  ? parseFloat(total).toLocaleString("it-IT", {
-                      style: "currency",
-                      currency: "VND",
-                      minimumFractionDigits: 3,
-                    })
-                  : "0 VND"}
-              </div>
-            </div>
-
-            <div
-              className="row"
-              style={{
-                // borderTop: "1px solid rgba(0,0,0,.1)",
-                padding: "2vh 0",
-              }}
-            >
-              <div className="col">Discount Rate</div>
-              <div className="col text-right">
-                {total > 0
-                  ? parseFloat(total).toLocaleString("it-IT", {
-                      style: "currency",
-                      currency: "VND",
-                      minimumFractionDigits: 3,
-                    })
-                  : "0 VND"}
-              </div>
-            </div>
-
-            <div
-              className="row"
-              style={{
-                // borderTop: "1px solid rgba(0,0,0,.1)",
-                padding: "2vh 0",
-              }}
-            >
-              <div className="col">Tax</div>
-              <div className="col text-right">
-                {total > 0
-                  ? parseFloat(total).toLocaleString("it-IT", {
-                      style: "currency",
-                      currency: "VND",
-                      minimumFractionDigits: 3,
-                    })
-                  : "0 VND"}
-              </div>
-            </div>
-
-            <div
-              className="row"
-              style={{
-                // borderTop: "1px solid rgba(0,0,0,.1)",
-                padding: "2vh 0",
-              }}
-            >
-              <div className="col">TOTAL PRICE</div>
-              <div className="col text-right">
-                {total > 0
-                  ? parseFloat(total).toLocaleString("it-IT", {
-                      style: "currency",
-                      currency: "VND",
-                      minimumFractionDigits: 3,
-                    })
-                  : "0 VND"}
-              </div>
-            </div>
-
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <button className="btn" onClick={purchase}>
-                Purchase
-              </button>
-            </div>
-          </div>
+          <Summary total={total} purchase={purchase} />
         </div>
       </div>
     </div>
---
+++ reactjs/src/components/Body/Layout/Cart/Summary/Summary.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+
+function Summary(props) {
+  const { total, purchase } = props;
+
+  const [showLabel, setShowLabel] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
+
+  const checkCode = (e) => {
+    var value = e.target.value.length;
+
+    switch (value) {
+      case 0: {
+        setShowLabel(true);
+        setFail(false);
+        setSuccess(false);
+        break;
+      }
+
+      case 10: {
+        setShowLabel(false);
+        setFail(false);
+        setSuccess(true);
+        break;
+      }
+
+      default:
+        setShowLabel(false);
+        setFail(true);
+        setSuccess(false);
+        break;
+    }
+  };
+
+  return (
+    <div className="col-md-4 summary">
+      <div>
+        <h5>
+          <b>Summary</b>
+        </h5>
+      </div>
+      <hr />
+
+      <form>
+        <p>GIVE CODE</p>
+
+        <div className="div_animation">
+          <input id="code" onChange={checkCode} />
+          <label
+            style={{ display: showLabel ? "block" : "none" }}
+            htmlFor="code"
+          ></label>
+        </div>
+      </form>
+
+      <div
+        // className="row"
+        style={{
+          padding: "2vh 0",
+        }}
+      >
+        <div
+          style={{
+            display: success ? "block" : "none",
+            color: "lightgreen",
+          }}
+          className="col"
+        >
+          Validated!
+        </div>
+        <div
+          className="col"
+          style={{
+            display: fail ? "block" : "none",
+            color: "red",
+          }}
+        >
+          Not Validated!
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          // borderTop: "1px solid rgba(0,0,0,.1)",
+          padding: "2vh 0",
+        }}
+      >
+        <div className="col">Original Price</div>
+        <div className="col text-right">
+          {total > 0
+            ? parseFloat(total).toLocaleString("it-IT", {
+                style: "currency",
+                currency: "VND",
+                minimumFractionDigits: 3,
+              })
+            : "0 VND"}
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          // borderTop: "1px solid rgba(0,0,0,.1)",
+          padding: "2vh 0",
+        }}
+      >
+        <div className="col">Discount Rate</div>
+        <div className="col text-right">
+          {total > 0
+            ? parseFloat(total).toLocaleString("it-IT", {
+                style: "currency",
+                currency: "VND",
+                minimumFractionDigits: 3,
+              })
+            : "0 VND"}
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          // borderTop: "1px solid rgba(0,0,0,.1)",
+          padding: "2vh 0",
+        }}
+      >
+        <div className="col">Tax</div>
+        <div className="col text-right">
+          {total > 0
+            ? parseFloat(total).toLocaleString("it-IT", {
+                style: "currency",
+                currency: "VND",
+                minimumFractionDigits: 3,
+              })
+            : "0 VND"}
+        </div>
+      </div>
+
+      <div
+        className="row"
+        style={{
+          // borderTop: "1px solid rgba(0,0,0,.1)",
+          padding: "2vh 0",
+        }}
+      >
+        <div className="col">TOTAL PRICE</div>
+        <div className="col text-right">
+          {total > 0
+            ? parseFloat(total).toLocaleString("it-IT", {
+                style: "currency",
+                currency: "VND",
+                minimumFractionDigits: 3,
+              })
+            : "0 VND"}
+        </div>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <button className="btn" onClick={purchase}>
+          Purchase
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Summary;
