5d16a589a5c02869047bef889f92b1b6aaedc931
your username <theibdn159555@gmail.com>
2021-08-18 20:28:31 +0700

    bugs tùm lum

--- laravel/app/Models/Product/Product.php
+++ laravel/app/Models/Product/Product.php
@@ -1,15 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Product;
 
-use App\Casts\Hash;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    // use HasFactory;
-
     protected $fillable = [
         "id",
         "category_id",
