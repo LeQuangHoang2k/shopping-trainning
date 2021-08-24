aeca33aeffdd3046666f2f3a5b4ffbd8036182a7
your username <theibdn159555@gmail.com>
2021-08-24 23:33:06 +0700

    fix UI dropdown

--- reactjs/src/components/Body/Layout/Order/AccountSideBar/AccountSideBar.jsx
+++ reactjs/src/components/Body/Layout/Order/AccountSideBar/AccountSideBar.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./AccountSideBar.css";
+import AccountSvg from "./svg/AccountSvg";
 
 function AccountSideBar(props) {
   return (
     <div>
       <div className="account_sidebar">
-        <div className="wrapper">account SideBAr</div>
+        {/* {} */}
+        <div className="wrapper">
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Thông tin tài khoản</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Thông báo của tôi</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Quản lý đơn hàng</span>
+          </a>
+        </div>
       </div>
     </div>
   );
---
+++ reactjs/src/components/Body/Layout/Order/AccountSideBar/svg/AccountSvg.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./AccountSidebarSvg.css";
+
+function AccountSvg(props) {
+  return (
+    <svg
+      className="account_sidebar_svg"
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      data-selected="true"
+      data-label-id="0"
+      data-metatip="true"
+    >
+      <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+    </svg>
+  );
+}
+
+export default AccountSvg;
