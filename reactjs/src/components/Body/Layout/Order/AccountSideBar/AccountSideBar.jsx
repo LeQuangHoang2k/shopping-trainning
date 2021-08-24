f0e67b7d584acc7238fe28cb05d25037ca68fc36
your username <theibdn159555@gmail.com>
2021-08-24 22:39:14 +0700

    fix UI dropdown

---
+++ reactjs/src/components/Body/Layout/Order/AccountSideBar/AccountSideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AccountSideBar.css";
+
+function AccountSideBar(props) {
+  return <div>12123</div>;
+}
+
+export default AccountSideBar;
--- reactjs/src/components/Body/Layout/Order/Order.jsx
+++ reactjs/src/components/Body/Layout/Order/Order.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Order.css";
 
 function Order(props) {
-  return <div>123</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <div className="account_sidebar">
+        <div className="wrapper">account SideBAr</div>
+      </div>
+      <div className="order_sidebar">
+        <div className="wrapper">List Ordered</div>
+      </div>
+    </div>
+  );
 }
 
 export default Order;
