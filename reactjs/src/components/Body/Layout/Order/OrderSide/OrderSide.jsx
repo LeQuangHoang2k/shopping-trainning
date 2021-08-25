c7bcdddcfeae8b06e7c3f3d2a10ab02e80d6178b
your username <theibdn159555@gmail.com>
2021-08-25 12:00:27 +0700

    create order list UI

--- reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
+++ reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
@@ -4,7 +4,18 @@ import "./OrderSide.css";
 function OrderSide(props) {
   return (
     <div className="order_side">
-      <div className="wrapper">List Ordered</div>
+      <div className="wrapper">
+        <div className="side_header">My order</div>
+        <table className="side_inner">
+          <tr className="" style={{ display: "flex" }}>
+            <th className="col side_title">Mã đơn hàng</th>
+            <th className="col side_title">Ngày mua</th>
+            <th className="col side_title">Sản phẩm</th>
+            <th className="col side_title">Tổng tiền</th>
+            <th className="col side_title">Trạng thái đơn hàng</th>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
