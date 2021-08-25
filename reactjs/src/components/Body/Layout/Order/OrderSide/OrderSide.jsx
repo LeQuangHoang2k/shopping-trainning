e821b0442b3b6df8667d7cb742d09eed2e7958d0
your username <theibdn159555@gmail.com>
2021-08-25 14:31:27 +0700

    fix UI order

--- reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
+++ reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
@@ -18,14 +18,16 @@ function OrderSide(props) {
           {[1, 2, 3, 4, 5, 6].map((key) => {
             return (
               <tr key={key} className="" style={{ display: "flex" }}>
-                <td className="col-2 side_title">414100606</td>
-                <td className="col-2 side_title">19/08/2021</td>
-                <td className="col-4 side_title">
+                <td className="col-2 side_content">
+                  <a href="/orders">#414100606</a>
+                </td>
+                <td className="col-2 side_content">19/08/2021</td>
+                <td className="col-4 side_content">
                   Sữa Rửa Mặt Cetaphil Gentle Skin Cleaner (500ml) -
                   8394107341305,9318637069637
                 </td>
-                <td className="col-2 side_title">306.850 ₫</td>
-                <td className="col-2 side_title">Bàn giao vận chuyển</td>
+                <td className="col-2 side_content">306.850 ₫</td>
+                <td className="col-2 side_content">Bàn giao vận chuyển</td>
               </tr>
             );
           })}
