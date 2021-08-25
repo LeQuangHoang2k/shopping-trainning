dbe62bbf0967e88631013e17c0dd35fea75384d5
your username <theibdn159555@gmail.com>
2021-08-25 14:54:04 +0700

    fix UI order

--- reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
+++ reactjs/src/components/Body/Layout/Order/OrderSide/OrderSide.jsx
@@ -7,7 +7,7 @@ function OrderSide(props) {
       <div className="wrapper">
         <div className="side_header">My order</div>
         <table className="side_inner">
-          <tr className="" style={{ display: "flex" }}>
+          <tr className="row" style={{ padding: "0", margin: "0" }}>
             <th className="col-2 side_title">Mã đơn hàng</th>
             <th className="col-2 side_title">Ngày mua</th>
             <th className="col-4 side_title">Sản phẩm</th>
@@ -17,16 +17,20 @@ function OrderSide(props) {
 
           {[1, 2, 3, 4, 5, 6].map((key) => {
             return (
-              <tr key={key} className="" style={{ display: "flex" }}>
+              <tr
+                key={key}
+                className="row"
+                style={{ padding: "0", margin: "0" }}
+              >
                 <td className="col-2 side_content">
-                  <a href="/orders">#414100606</a>
+                  <a className="side_id" href="/orders">#414100606</a>
                 </td>
                 <td className="col-2 side_content">19/08/2021</td>
                 <td className="col-4 side_content">
                   Sữa Rửa Mặt Cetaphil Gentle Skin Cleaner (500ml) -
                   8394107341305,9318637069637
                 </td>
-                <td className="col-2 side_content">306.850 ₫</td>
+                <td className="col-2 side_content">306.850 VND</td>
                 <td className="col-2 side_content">Bàn giao vận chuyển</td>
               </tr>
             );
