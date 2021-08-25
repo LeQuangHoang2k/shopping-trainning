5846d83eb6ebe064b2530d9cff27e52c01a1c585
your username <theibdn159555@gmail.com>
2021-08-25 15:17:02 +0700

    fix UI order

--- reactjs/src/components/Body/Layout/Order/AccountSideBar/AccountSideBar.jsx
+++ reactjs/src/components/Body/Layout/Order/AccountSideBar/AccountSideBar.jsx
@@ -33,6 +33,54 @@ function AccountSidebar(props) {
             <AccountSvg />
             <span>Quản lý đơn hàng</span>
           </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Số địa chỉ</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Thông tin thanh toán</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Nhận xét sản phẩm đã mua</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Sản phẩm bạn đã xem</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Sản phẩm yêu thích</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Sản phẩm mua sau</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Nhận xét của tôi</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Hỏi đáp</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Thông tin TikiNOW</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Mã giảm giá</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>Quản lý Tiki Xu của tôi</span>
+          </a>
+          <a href="abc" className="sidebar_item">
+            <AccountSvg />
+            <span>BookCare của tôi</span>
+          </a>
         </div>
       </div>
     </div>
