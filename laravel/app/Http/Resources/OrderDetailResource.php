dad3bafcb3ab8e8a5ee123b116d4b9e4dc958a81
theibdn159555 <theibdn159555@gmail.com>
2021-09-08 18:50:46 +0700

    fix orderdetails time

--- laravel/app/Http/Resources/OrderDetailResource.php
+++ laravel/app/Http/Resources/OrderDetailResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderDetailResource extends JsonResource
@@ -14,6 +15,14 @@ class OrderDetailResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'product_id' => $this->product_id,
+            'amount' => $this->amount,
+            'product_option_id' => $this->product_option_id,
+            'price_per_unit' => $this->price_per_unit,
+            'created_at' => Carbon::parse($this->created_at)->format('d/m/Y'),
+        ];
     }
 }
