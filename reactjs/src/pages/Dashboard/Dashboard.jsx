6ecd35dd3bd7da1be628eb8b00924e7992694919
Long <vuduclong1710@gmail.com>
2022-02-27 15:33:48 +0700

    main

--- reactjs/src/pages/Dashboard/Dashboard.jsx
+++ reactjs/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,77 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+import { useHistory } from "react-router-dom";
 
 function Dashboard(props) {
-    return (
-        <div>
-            abc
+  const cookies = new Cookies();
+  const [user, setUser] = useState(cookies.get("user"));
+  let history = useHistory();
+
+  useEffect(() => {
+    console.log("user is: ", user);
+    if (!user || user.role !== "admin") history.push("/");
+    return () => {};
+  }, []);
+  // product (them,sua,xoa)
+  // user (them,sua,xoa,list)
+  // discount (them,sua,xoa)
+  // order
+  // category
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <div
+        style={{
+          width: "20%",
+          height: "100vh",
+          backgroundColor: "#122D4F",
+          float: "left",
+          color: "white",
+        }}
+      >
+        123
+      </div>
+      <div
+        style={{
+          width: "80%",
+          height: "100vh",
+          backgroundColor: "#F5F5F4",
+          float: "right",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "80px",
+            backgroundColor: "white",
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: "50px",
+            fontSize: "20px",
+            fontFamily: "helvetica",
+            fontWeight : "bold"
+          }}
+        >
+          DashBoard
         </div>
-    );
+        <div
+          style={{ width: "100%", height: "auto", backgroundColor: "#F5F5F4" }}
+        >
+          <div
+            style={{
+              width: "1000px",
+              height: "500px",
+              backgroundColor: "white",
+              margin: "auto",
+              marginTop: "50px",
+              borderRadius: "15px",
+            }}
+          >
+            789
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
--- reactjs/src/pages/Home/Home.jsx
+++ reactjs/src/pages/Home/Home.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-
-import Navbar from '../../components/Navbar/Navbar';
-import Body from '../../components/Body/Body';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Cookies from "universal-cookie";
+import Navbar from "../../components/Navbar/Navbar";
+import Body from "../../components/Body/Body";
 // import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
 
-
 function Home(props) {
-    return (
-        <div>
-            <Navbar/>  
-            {/* <Breadcrumb />  */}
-            <Body />
-        </div>
-    );
+  const cookies = new Cookies();
+  const [user, setUser] = useState(cookies.get("user"));
+  let history = useHistory();
+
+  useEffect(() => {
+    console.log("user is: ", user);
+    if (user && user.role === "admin") history.push("/dashboard");
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      {/* <Breadcrumb />  */}
+
+      <Body />
+    </div>
+  );
 }
 
 export default Home;
